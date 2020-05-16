75906a9ec864039aad56c05d443c9a928793767d
Anton Shvets <antshvets@gmail.com>
2020-05-16 20:13:07 +0300

    month-container tests

--- projects/datepicker/src/lib/month-container/month-container.component.spec.ts
+++ projects/datepicker/src/lib/month-container/month-container.component.spec.ts
@@ -1,9 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MonthContainerComponent } from './month-container.component';
-import { MockComponent } from 'ng-mocks';
+import { MockComponent, MockedComponent } from 'ng-mocks';
 import { MonthComponent } from '../month/month.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { AnimationEvent } from '@angular/animations';
 
 describe('YearComponent', () => {
   let component: MonthContainerComponent;
@@ -13,7 +15,7 @@ describe('YearComponent', () => {
     TestBed.configureTestingModule({
       declarations: [MonthContainerComponent, MockComponent(MonthComponent)],
       imports: [
-        NoopAnimationsModule,
+        BrowserAnimationsModule,
       ]
     })
       .compileComponents();
@@ -26,5 +28,75 @@ describe('YearComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have default parameters', () => {
+    expect(component.nextMonth).toBe(0);
+    expect(component.animationState).toBe('stable');
+  });
+
+  it('should contain two slim-month', () => {
+    fixture.detectChanges();
+    const slimMonthDE = fixture.debugElement.queryAll(By.directive(MonthComponent));
+    expect(slimMonthDE.length).toBe(2);
+  });
+
+  it('should monthes gets inputs parameters', () => {
+    component.year = 1005;
+    component.month = 5;
+    component.nextMonth = 4;
+    fixture.detectChanges();
+
+    const flying = fixture.debugElement.query(By.css('.flying-month')).componentInstance;
+    const current = fixture.debugElement.query(By.css('.current-month')).componentInstance;
+
+    expect(flying.month).toBe(component.nextMonth);
+    expect(flying.year).toBe(component.year);
+
+    expect(current.month).toBe(component.month);
+    expect(current.year).toBe(component.year);
+  });
+
+  it('should change animationState and nextMonty after getting positive monthTurn', () => {
+    component.month = 5;
+    component.monthTurn = { turn: 1 };
+    expect(component.nextMonth).toBe(6);
+    expect(component.animationState).toBe('next');
+  });
+
+  it('should change animationState and nextMonty after getting negative monthTurn', () => {
+    component.month = 5;
+    component.monthTurn = { turn: -1 };
+    expect(component.nextMonth).toBe(4);
+    expect(component.animationState).toBe('previous');
+  });
+
+  it('should call onCurrentDone after setting animationState "next"', async () => {
+    spyOn(component, 'onCurrentDone');
+    component.animationState = 'next';
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(component.onCurrentDone).toHaveBeenCalled();
+  });
+
+  it('should call onCurrentDone after setting animationState "previous"', async () => {
+    spyOn(component, 'onCurrentDone');
+    component.animationState = 'previous';
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(component.onCurrentDone).toHaveBeenCalled();
+  });
+
+  it('should change animationState and nextMonth after call onCurrentDone', async () => {
+    let emittedNextMonth = null;
+    const evt = { toState: 'previous' } as AnimationEvent;
+    component.nextMonthChange.subscribe(n => emittedNextMonth = n);
+    component.animationState = 'previous';
+    component.nextMonth = 11;
+    component.onCurrentDone(evt);
+
+    expect(emittedNextMonth).toBe(11);
+    expect(component.nextMonth).toBe(0);
+    expect(component.animationState).toBe('stable');
   });
 });
